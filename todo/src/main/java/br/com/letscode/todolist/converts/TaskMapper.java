ce9bd018090b6907b89383c6f65ed7d8e937679c
Rumenigg <rumenigg@gmail.com>
2022-05-11 22:45:31 -0300

    Added Dockerfile and docker-compose.yml

--- todo/src/main/java/br/com/letscode/todolist/converts/TaskMapper.java
+++ todo/src/main/java/br/com/letscode/todolist/converts/TaskMapper.java
@@ -5,7 +5,7 @@ import br.com.letscode.todolist.dto.TaskDTO;
 import br.com.letscode.todolist.model.Task;
 import org.mapstruct.Mapper;
 
-import javax.transaction.Transactional;
+
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
